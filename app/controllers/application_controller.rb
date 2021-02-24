5e08afc3b3c5bb30acaa714484f2969a706fe038
teke <takehiro.arai.jg@gmail.com>
2021-02-24 09:01:19 +0900

    6-2 500 Internal Server Error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   layout :set_layout
 
+  rescue_from StandardError, with: :rescue500
+
   private def set_layout
     if params[:controller].match(%r{\A(staff|admin|customer)/})
       Regexp.last_match[1]
@@ -9,4 +11,7 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  private def rescue500(e)
+    render "errors/internal_server_error", status: 500
+  end
 end
